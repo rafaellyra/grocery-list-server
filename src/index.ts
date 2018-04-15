32d5aeb17aa3046b1779564eff96ccc5b1202eb8
Rafael Lyra <rafael@rafaellyra.com>
2018-04-15 21:37:50 +0200

    Login - code refactor

--- src/index.ts
+++ src/index.ts
@@ -12,11 +12,12 @@ import { makeExecutableSchema } from 'graphql-tools'
 import * as path from 'path'
 
 // App imports
-import { Routes } from './routes'
+import {LoginRoutes, RegisterRoutes,} from './routes'
 
 // Authentication impots
 import * as passport from 'passport'
 import Account from './models/account'
+import {Register} from "ts-node";
 
 const app = express()
 
@@ -40,7 +41,11 @@ app.use(require('express-session')({
 
 app.use(passport.initialize())
 app.use(passport.session())
-app.use('/login', Routes)
+
+app.options('*', cors())
+app.use(express.json())
+app.use(LoginRoutes)
+app.use(RegisterRoutes)
 
 // Passport config
 passport.use(new LocalStrategy(Account.authenticate()));
@@ -50,41 +55,6 @@ passport.deserializeUser(Account.deserializeUser());
 // App initialization
 app.use('/graphql', bodyParser.json(), cors(), graphqlExpress({ schema }))
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
-
-app.options('/login', cors())
-app.use('/login', express.json())
-app.post('/login', cors(), function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-
-    passport.authenticate('local', function(err, user) {
-        if (err) { return next(err) }
-        if (!user) { return res.send('User not found'); }
-        req.logIn(user, function(err) {
-            if (err) { return next(err); }
-            return res.send(user);
-        });
-    })(req, res, next);
-})
-
-app.options('/register', cors())
-app.use('/register', express.json())
-app.post('/register', cors(), function(req, res, next) {
-    Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
-        if (err) {
-            return res.status(500).send(err)
-        }
-
-        passport.authenticate('local', function(err, user) {
-            if (err) { return next(err) }
-            if (!user) { return res.send('User not found'); }
-            req.logIn(user, function(err) {
-                if (err) { return next(err); }
-                return res.send(user);
-            });
-        })(req, res, next);
-    });
-});
 
 app.listen(9001, () => {
     console.log('GraphQL server is running on http://localhost:9001/graphql')
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,27 +1,2 @@
-import * as cors from 'cors'
-import { Router, Request, Response } from 'express'
-import * as passport from 'passport'
-const router: Router = Router()
-
-/* GET home page. */
-router.post('/login', cors(), login)
-
-router.options('/login', cors())
-// router.use('/login', express.json())
-
-
-function login(req: Request, res: Response, next) {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-
-    passport.authenticate('local', function(err, user) {
-        if (err) { return next(err) }
-        if (!user) { return res.send('User not found'); }
-        req.logIn(user, function(err) {
-            if (err) { return next(err); }
-            return res.send(user);
-        });
-    })(req, res, next);
-}
-
-export const Routes: Router = router
+export { LoginRoutes } from './login'
+export { RegisterRoutes } from './register'
